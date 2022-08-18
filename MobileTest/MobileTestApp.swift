de24ac5eecf688d033d4822d69c9a18b151f1f89
Rafael Custódio Silva <19rafaelcsilva@gmail.com>
2022-08-18 15:39:08 -0300

    Initial Commit

---
+++ MobileTest/MobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTestApp.swift
+//  MobileTest
+//
+//  Created by Rafael Custódio Silva on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
